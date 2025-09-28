08c0e84c373e6297d167ed2a6eb89c2d1dd54aca
Aalok Hasabnis <aalokhasabnis13@gmail.com>
2025-09-28 14:19:54 +0530

    feat: integrate geolocation into WeatherDashboard with refresh and loading skeleton

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,6 +1,36 @@
+import WeatherSkelton from "@/components/loading-skleton";
+import { Button } from "@/components/ui/button"
+import { useGeolocation } from "@/hooks/use-geolocation"
+import { RefreshCw } from "lucide-react"
+
 const WeatherDashBoard = () => {
+
+  const {coordinates, error:locationError, getLocation, isLoading:locationLoading} = useGeolocation();
+
+  console.log(coordinates);
+
+  const handleRefresh = () => {
+    getLocation();
+    if(coordinates) {
+      //reload weather data based on new coordinates
+    }
+  };
+
+  if(locationLoading) {
+    return <WeatherSkelton />
+  }
+
   return (
-    <div>WeatherDashBoard</div>
+    <div className="space y-4">
+      {/*Favorite cities list */}
+      <div  className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tightly"> My Location </h1>
+          
+          <Button variant={'outline'} size={'icon'} className="ml-2" onClick={handleRefresh}>
+            <RefreshCw className="h-4 w-4"/>
+          </Button>
+      </div>
+    </div>
   )
 }
 
